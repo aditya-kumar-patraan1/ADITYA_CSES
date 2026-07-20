9d6a100a4ebff9c5053ae225a0f92a14f4fef48e
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-07-20 21:19:55 +0530

    partial solution for minimal grid path added -Dynamic Programming

---
+++ MATHS/sum_of_divisors.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const int MOD = 1000000007;
+
+int main()
+{
+
+    ll n;
+    cin >> n;
+
+    int ans = 0LL;
+
+    for (long long d = 1; d <= n; d++)
+        ans += d * (n / d);
+
+    cout << ans << endl;
+}
